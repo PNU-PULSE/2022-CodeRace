1dd437ae278fce7e6abeba961616ee949175c377
Sang Un Park <sangwoon.alde@pusan.ac.kr>
2022-08-16 21:18:49 +0900

    pqbd 솔루션 오답 수정

--- 풀이/pqbd.py
+++ 풀이/pqbd.py
@@ -1,65 +1,15 @@
 mirror = {}
 turn = {}
 
-mirror['a'] = '.'
-mirror['b'] = 'd'
-mirror['c'] = '.'
-mirror['d'] = 'b'
-mirror['e'] = '.'
-mirror['f'] = '.'
-mirror['g'] = '.'
-mirror['h'] = '.'
-mirror['i'] = 'i'
-mirror['j'] = '.'
-mirror['k'] = '.'
-mirror['l'] = 'l'
-mirror['m'] = 'm'
-mirror['n'] = 'n'
-mirror['o'] = 'o'
-mirror['p'] = 'q'
-mirror['q'] = 'p'
-mirror['r'] = '.'
-mirror['s'] = '.'
-mirror['t'] = '.'
-mirror['u'] = '.'
-mirror['v'] = 'v'
-mirror['w'] = 'w'
-mirror['x'] = 'x'
-mirror['y'] = '.'
-mirror['z'] = '.'
-mirror['^'] = '$'
-mirror['$'] = '^'
-mirror['#'] = '#'
+ALPHABET =      'abcdefghijklmnopqrstuvwxyz^$#'
+MIRROR_STR =    '.d.b....i..lmnoqp....vwx..$^#'
+TURN_STR =      '.q.p.......l..odb.s....x.z$^#'
 
-turn['a'] = '.'
-turn['b'] = 'q'
-turn['c'] = '.'
-turn['d'] = 'p'
-turn['e'] = '.'
-turn['f'] = '.'
-turn['g'] = '.'
-turn['h'] = '.'
-turn['i'] = '.'
-turn['j'] = '.'
-turn['k'] = '.'
-turn['l'] = 'l'
-turn['m'] = '.'
-turn['n'] = '.'
-turn['o'] = 'o'
-turn['p'] = 'd'
-turn['q'] = 'b'
-turn['r'] = '.'
-turn['s'] = 's'
-turn['t'] = '.'
-turn['u'] = '.'
-turn['v'] = '.'
-turn['w'] = '.'
-turn['x'] = 'x'
-turn['y'] = '.'
-turn['z'] = 'z'
-turn['^'] = '$'
-turn['$'] = '^'
-turn['#'] = '#'
+for _key, _value in zip(ALPHABET, MIRROR_STR):
+    mirror[_key] = _value
+    
+for _key, _value in zip(ALPHABET, TURN_STR):
+    turn[_key] = _value
 
 def longestMirrorPalindrome(s):
     # Transform S into T.
@@ -73,8 +23,9 @@ def longestMirrorPalindrome(s):
         P[i] = (R > i) and min(R - i, P[2*C - i]) # equals to i' = C - (i-C)
         # Attempt to expand palindrome centered at i
         
-        while i - 1 - P[i] >= 0 and T[i + 1 + P[i]] == mirror[T[i - 1 - P[i]]]:
-            P[i] += 1
+        if T[i] == mirror[T[i]]:
+            while i - 1 - P[i] >= 0 and T[i + 1 + P[i]] == mirror[T[i - 1 - P[i]]]:
+                P[i] += 1
 
         # If palindrome centered at i expand past R,
         # adjust center based on expanded palindrome.
@@ -98,8 +49,9 @@ def longestTurnPalindrome(s):
     for i in range (1, n-1):
         P[i] = (R > i) and min(R - i, P[2*C - i]) # equals to i' = C - (i-C)
         # Attempt to expand palindrome centered at i
-        while i - 1 - P[i] >= 0 and T[i + 1 + P[i]] == turn[T[i - 1 - P[i]]]:
-            P[i] += 1
+        if T[i] == turn[T[i]]:
+            while i - 1 - P[i] >= 0 and T[i + 1 + P[i]] == turn[T[i - 1 - P[i]]]:
+                P[i] += 1
 
         # If palindrome centered at i expand past R,
         # adjust center based on expanded palindrome.
@@ -116,5 +68,8 @@ def longestTurnPalindrome(s):
 # exp_str = "aaapqbdcksppddxbboqqppodd"
 exp_str = input().strip()
 
-
-print(max([longestTurnPalindrome(exp_str),longestMirrorPalindrome(exp_str)], key=len))
+answer = max([longestTurnPalindrome(exp_str),longestMirrorPalindrome(exp_str)], key=len)
+if answer:
+    print(answer)
+else:
+    print("NOANSWER")
